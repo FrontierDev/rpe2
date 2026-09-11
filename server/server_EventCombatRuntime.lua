37c3aa85690d1e6410e15f7d9859984adfb32bdc
ortellus <elvetgames@gmail.com>
2026-09-11 23:07:09 +0100

    Remove abandoned event combat runtime module

--- server/server_EventCombatRuntime.lua
+++
@@ -1,6 +0,0 @@
-local _, Addon = ...
-
--- Host-authoritative combat mutation wrapping disabled.
--- Aura/cast/cooldown/defensive traffic stays on the established gameplay paths.
-Addon.Server = Addon.Server or {}
-return Addon.Server
