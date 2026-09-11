186697c726dc3d7a25c65b51fb0fb4ed24bec28d
ortellus <elvetgames@gmail.com>
2026-09-11 23:07:41 +0100

    Remove abandoned event revision runtime module

--- server/server_EventRuntime.lua
+++
@@ -1,6 +0,0 @@
-local _, Addon = ...
-
--- Event-rejoin revision/mutation runtime disabled.
--- Normal event replication remains on the existing server/client domain paths.
-Addon.Server = Addon.Server or {}
-return Addon.Server
