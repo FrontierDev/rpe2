f9f61805f4f1333fd8a9bd0b7fb15ec94caa592a
ortellus <elvetgames@gmail.com>
2026-09-11 23:07:36 +0100

    Remove abandoned rejoin server module

--- server/server_EventRejoinSync.lua
+++
@@ -1,6 +0,0 @@
-local _, Addon = ...
-
--- Superseded snapshot/revision rejoin integration disabled.
--- Server:ReconcileClientEventSession() now runs its original event snapshot path.
-Addon.Server = Addon.Server or {}
-return Addon.Server
