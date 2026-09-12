3cd269209d24ec4ef121f225a55dd73daddb8ada
ortellus <elvetgames@gmail.com>
2026-09-12 19:15:10 +0100

    Update #266 Guild Setting editor terminology

--- client/ui/editor/pages/page_GuildSetting.lua
+++ client/ui/editor/pages/page_GuildSetting.lua
@@ -119,9 +119,9 @@ function DataEditor:RefreshGuildSettingDataPage()
 
     if self.GuildSettingPageEmptyText and self.GuildSettingPageEmptyText.SetText then
         if not dataset then
-            self.GuildSettingPageEmptyText:SetText("Create or select a dataset to view Guild Ranks.")
+            self.GuildSettingPageEmptyText:SetText("Create or select a dataset to view Guild Settings.")
         elseif #guildSettings == 0 then
-            self.GuildSettingPageEmptyText:SetText("This dataset has no Guild Ranks yet.")
+            self.GuildSettingPageEmptyText:SetText("This dataset has no Guild Settings yet.")
         else
             self.GuildSettingPageEmptyText:SetText("")
         end
