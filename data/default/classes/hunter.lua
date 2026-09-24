28ac9b7437bbfdd74793db9f5a1dfb74a7f0a9a3
ortellus <elvetgames@gmail.com>
2026-09-24 11:16:16 +0100

    Add Hunter class scaffold

---
+++ data/default/classes/hunter.lua
@@ -0,0 +1,65 @@
+local _, Addon = ...
+
+Addon.Data.DefaultDatasets:Register({
+    version = 1,
+    dataset = {
+        achievements = {},
+        auras = {},
+        authorName = "Ortellus-ArgentDawn",
+        classes = {
+            {
+                description = "",
+                icon = "interface/icons/classicon_hunter.blp",
+                id = "h7n4t2er",
+                name = "Hunter",
+                armorWeights = {
+                    "leather",
+                    "mail",
+                },
+                weaponTypeRefs = {
+                    "f82db71a:z6nh3znw",
+                    "f82db71a:9ni3vfas",
+                    "f82db71a:y0dnlo8g",
+                    "f82db71a:25s5wyry",
+                    "f82db71a:3y1v01e4",
+                    "f82db71a:l3ce0puc",
+                    "f82db71a:j2gceby4",
+                    "f82db71a:anoo8qfp",
+                    "f82db71a:gjz2331m",
+                    "f82db71a:we5ul4ne",
+                },
+                resourceProgressions = {},
+                skillBonuses = {},
+                statProgressions = {},
+                passiveTraitRefs = {},
+                talentTraitRefs = {},
+            }
+        },
+        currencies = {},
+        damageSchools = {},
+        datasetType = "class",
+        dependencies = {
+            "f82db71a"
+        },
+        description = "",
+        groupName = "Core",
+        guildSettings = {},
+        id = "a93f7c12",
+        interactions = {},
+        itemSlots = {},
+        items = {},
+        loot = {},
+        mounts = {},
+        name = "Hunter",
+        pets = {},
+        races = {},
+        recipes = {},
+        resources = {},
+        skills = {},
+        spells = {},
+        stats = {},
+        traits = {},
+        units = {},
+        weaponTypes = {}
+    },
+})
